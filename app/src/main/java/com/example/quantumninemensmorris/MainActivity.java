d461b0e15478102fb8a9874d357ee4b9367ec3b1
Hesusu-ikie <jesuspaezpaez@gmail.com>
2023-04-15 14:33:43 +0200

    Added Game view

--- app/src/main/java/com/example/quantumninemensmorris/MainActivity.java
+++ app/src/main/java/com/example/quantumninemensmorris/MainActivity.java
@@ -2,18 +2,41 @@ package com.example.quantumninemensmorris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPlay, btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button myButton = findViewById(R.id.BtnPlay);
-        myButton.setBackgroundColor(getResources().getColor(R.color.palette_blue));
+        btnPlay = findViewById(R.id.BtnPlay);
+        btnExit = findViewById(R.id.BtnExit);
 
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Iniciando el juego...");
+                initGame();
+            }
+        });
+
+        btnExit.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               finish();
+           }
+       });
+    }
+
+    private void initGame() {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
     }
 }
