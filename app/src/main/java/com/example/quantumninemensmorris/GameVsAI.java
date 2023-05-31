16027a386e918b3f3ba90024dd01cfa8e548452b
Nuria Gómez <80064342+nurgomari@users.noreply.github.com>
2023-05-31 20:59:50 +0200

    Eval function update

--- app/src/main/java/com/example/quantumninemensmorris/GameVsAI.java
+++ app/src/main/java/com/example/quantumninemensmorris/GameVsAI.java
@@ -1420,7 +1420,7 @@ public class GameVsAI extends Activity {
     private Integer evaluateBoard(ArrayList<String> board, String player) {
         Integer res = 0;
 
-        res = (int) Math.round(EvalFunction.f3(board, player) * 3/12 + 6/12 * EvalFunction.f11(board, player) + 0/12 * EvalFunction.f12(board, player) + 0/12 * EvalFunction.f14(board, player));
+        res = (int) Math.round(EvalFunction.f10(board, player) * 1/5 + EvalFunction.f11(board, player) * 1/5 + EvalFunction.f12(board, player) * 1/5 + EvalFunction.f13(board, player) * 1/5 + EvalFunction.f14(board, player) * 1/5);
         System.out.println();
 //                (int) Math.round(1/6 * EvalFunction.f4(board, player) + 1/6 * EvalFunction.f5(board, player) + 1/6 * EvalFunction.f6(board, player) +
 //                        1/6 * EvalFunction.f7(board, player) + 1/6 * EvalFunction.f8(board, player) + 1/6 * EvalFunction.f9(board, player));
